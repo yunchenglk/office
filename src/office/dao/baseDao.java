bf31d65e986b19c26486173b1def77f833622c4c
Administrator <Administrator@192.168.0.105>
2017-01-02 11:26:02 +0800

    1

--- src/office/dao/baseDao.java
+++ src/office/dao/baseDao.java
@@ -1,5 +1,94 @@
 package office.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import office.util.ConfigManager;
+
 public class baseDao {
+	private Connection conn;
+	private ResultSet rs;
+	private PreparedStatement ps;
+
+	public boolean getConnection() {
+		try {
+			Class.forName(ConfigManager.instance().getVal("jdbc.class"));
+			conn = DriverManager.getConnection(ConfigManager.instance().getVal("jdbc.conn.url"),
+					ConfigManager.instance().getVal("jdbc.conn.username"),
+					ConfigManager.instance().getVal("jdbc.conn.password"));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	public ResultSet executeQuery(String sql, Object[] params) {
+		if (getConnection()) {
+			try {
+				ps = conn.prepareStatement(sql);
+				int i = 0;
+				for (Object o : params) {
+					ps.setObject(++i, o);
+				}
+				rs = ps.executeQuery();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return rs;
+	}
+
+	public int executeUpdate(String sql, Object[] params) {
+		int updateRow = 0;
+		if (getConnection()) {
+			try {
+				ps = conn.prepareStatement(sql);
+				int i = 0;
+				for (Object o : params) {
+					ps.setObject(++i, o);
+				}
+				updateRow = ps.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return updateRow;
+
+	}
+	public boolean closeResouce(){
+		if(ps != null){
+			try {
+				ps.close();
+			} catch (SQLException e) { 
+				e.printStackTrace();
+				return false;
+			}
+		}
+		if(rs != null){
+			try {
+				rs.close();
+			} catch (SQLException e) { 
+				e.printStackTrace();
+				return false;
+			}
+		}
+		if(conn != null){
+			try {
+				conn.close();
+			} catch (SQLException e) { 
+				e.printStackTrace();
+				return false;
+			}
+		}
+		return true;
+		
+	}
 
 }
---
+++ src/office/util/ConfigManager.java
@@ -0,0 +1,36 @@
+package office.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigManager {
+	private static ConfigManager config;
+	private static Properties properties;
+
+	private ConfigManager() {
+		try {
+			properties = new Properties();
+			InputStream is = ConfigManager.class.getResourceAsStream("database.properties");
+			properties.load(is);
+			is.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static ConfigManager instance() {
+		if (config == null) {
+			new ConfigManager();
+		}
+		return config;
+	}
+
+	public String getVal(String key) {
+		return properties.getProperty(key);
+
+	}
+
+}
